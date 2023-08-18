cebd03e4e2b2e19c92e3d76c849b5741fcaa78b6
MikLay <mfediuchenko@zgallerie.com>
2023-08-18 20:29:34 +0300

    Update deadline

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './App.css'
 import CountDown from './components/CountDown/Countdown'
 
-const UPDATE_DEADLINE = 'August 18, 2023'
+const UPDATE_DEADLINE = 'August 20, 2023'
 const App: React.FC = () => {
   return (
         <div className="App">
@@ -10,7 +10,7 @@ const App: React.FC = () => {
                 <h1>Інформація стане доступною через</h1>
                 <CountDown targetDate={UPDATE_DEADLINE}>
                     <div>
-                        <span>Ready today</span></div>
+                        <span>Запуск на цих вихідних (19-20 серпня)</span></div>
                 </CountDown>
             </header>
         </div>
